9dde8793c631503a432681229c80b1f7876ed7eb
Tomasz Miller <info@tomaszmiller.pl>
2017-05-31 19:24:33 +0200

    Optional added

--- src/main/java/pl/tomaszmiller/AppNo2/TicketRepository.java
+++ src/main/java/pl/tomaszmiller/AppNo2/TicketRepository.java
@@ -4,12 +4,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.tomaszmiller.AppNo2.models.Ticket;
 
+import java.util.Optional;
+
 /**
  * Created by Peniakoff on 31.05.2017.
  */
 @Repository
 public interface TicketRepository extends CrudRepository<Ticket, Integer> {
 
-
+    Optional<Ticket> findOne(int id); //overwrite the findOne method
 
 }
--- src/main/java/pl/tomaszmiller/AppNo2/controllers/MainController.java
+++ src/main/java/pl/tomaszmiller/AppNo2/controllers/MainController.java
@@ -2,11 +2,14 @@ package pl.tomaszmiller.AppNo2.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.tomaszmiller.AppNo2.TicketRepository;
 import pl.tomaszmiller.AppNo2.models.Ticket;
+
+import java.util.Optional;
 
 /**
  * Created by Peniakoff on 31.05.2017.
@@ -17,12 +20,14 @@ public class MainController {
     @Autowired
     TicketRepository ticketRepository;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/{ticketId}", method = RequestMethod.GET)
     @ResponseBody
-    public String home() {
-        Ticket ticket = new Ticket("To jest przykładowa wiadomość.", "Tomasz M.");
-        ticketRepository.save(ticket);
-        return "Zapisano do bazy danych.";
+    public String home(@PathVariable("ticketId") int id) {
+        Optional ticket = ticketRepository.findOne(id); //Optional for exceptions management
+        if (ticket.isPresent()) {
+            return "The message form the DB is: " + ticket.get().getMessage(); //it returns the message with ID in the url
+        }
+        return "Wrong ID used!";
     }
 
 
