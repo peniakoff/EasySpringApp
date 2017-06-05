f01e2924d060b249265b83ef7f12148084bf4064
Tomasz Miller <info@tomaszmiller.pl>
2017-06-05 20:28:59 +0200

    Optional findByUsername added

--- src/main/java/pl/tomaszmiller/AppNo2/UserRepository.java
+++ src/main/java/pl/tomaszmiller/AppNo2/UserRepository.java
@@ -8,6 +8,7 @@ import pl.tomaszmiller.AppNo2.models.User;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by Peniakoff on 03.06.2017.
@@ -18,5 +19,6 @@ public interface UserRepository extends CrudRepository<User, Integer> {
     List<User> findByDatetimeBetween(Date date1, Date date2);
     List<User> findByUsernameContainingAndIdGreaterThan(String reg, int id);
     Page<User> findAll(Pageable pageable);
+    Optional<User> findByUsername(String name);
 
 }
