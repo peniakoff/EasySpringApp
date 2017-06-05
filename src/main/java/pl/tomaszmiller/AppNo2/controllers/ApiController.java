b24ee50d33c76a7007c1beb80dbcfdb4095aa091
Tomasz Miller <info@tomaszmiller.pl>
2017-06-05 18:15:29 +0200

    ApiController created

---
+++ src/main/java/pl/tomaszmiller/AppNo2/controllers/ApiController.java
@@ -0,0 +1,13 @@
+package pl.tomaszmiller.AppNo2.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Peniakoff on 05.06.2017.
+ */
+@Controller
+public class ApiController {
+
+
+
+}
