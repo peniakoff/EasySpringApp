6bb4a385680f42f736a47b6ddc68a583cb364e7c
Tomasz Miller <info@tomaszmiller.pl>
2017-06-10 12:56:02 +0200

    files added

---
+++ src/main/java/pl/tomaszmiller/AppNo2/LoginHandler.java
@@ -0,0 +1,7 @@
+package pl.tomaszmiller.AppNo2;
+
+/**
+ * Created by Peniakoff on 10.06.2017.
+ */
+public class LoginHandler {
+}
---
+++ src/main/java/pl/tomaszmiller/AppNo2/controllers/SecureController.java
@@ -0,0 +1,7 @@
+package pl.tomaszmiller.AppNo2.controllers;
+
+/**
+ * Created by Peniakoff on 10.06.2017.
+ */
+public class SecureController {
+}
